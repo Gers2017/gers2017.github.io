1cd73055a6ef32f5fd64c1ce2a8f4f961bb9fb3b
Gers2017 <Gers2017@outlook.com>
2022-08-07 22:35:04 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://Gers2017.github.io",
+    base: "/gers2017.github.io"
+});
