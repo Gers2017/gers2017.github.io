4385253df248152f4832e1c03fe98f97f2122fa2
Gers2017 <Gers2017@outlook.com>
2022-08-08 09:59:16 -0600

    Move styles to public directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://Gers2017.github.io",
-    base: "/gers2017.github.io"
+    site: "https://gers2017.github.io",
 });
