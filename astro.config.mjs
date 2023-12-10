70ace526a759da855cfa20b647e5ab99331898fe
Gers2017 <Gers2017@outlook.com>
2023-12-09 20:45:14 -0600

    chore(config): format astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gers2017.github.io",
-  markdown: {
-    shikiConfig: {
-      theme: "github-dark-dimmed"
-    }
-  },
-  integrations: [solidJs()]
+    site: "https://gers2017.github.io",
+    markdown: {
+        shikiConfig: {
+            theme: "github-dark-dimmed",
+        },
+    },
+    integrations: [solidJs()],
 });
